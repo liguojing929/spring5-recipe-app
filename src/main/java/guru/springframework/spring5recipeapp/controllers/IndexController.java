08253517517e384636ec0d60374b4170447a1a79
liguojing929 <dannyli0818@protonmail.com>
2020-03-13 21:47:13 +0800

    enable live reload functionality. closes #4

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -2,13 +2,21 @@ package guru.springframework.spring5recipeapp.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        System.out.println("Some Message to say...hello");
         return "index";
     }
 }
