36acee335de2de55c7855efa97eb890f09a02d92
liguojing929 <dannyli0818@protonmail.com>
2020-03-14 23:08:16 +0800

    complete assignment.

---
+++ src/main/java/guru/springframework/spring5recipeapp/model/UnitOfMeasure.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring5recipeapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
