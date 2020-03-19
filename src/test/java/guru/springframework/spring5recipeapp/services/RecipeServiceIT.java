7c41c8a7d32202571ff4847617347ea360df8594
liguojing929 <dannyli0818@protonmail.com>
2020-03-19 22:15:27 +0800

    Add Command Object and Type Conversion in Spring.

--- src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceIT.java
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceIT.java
@@ -7,12 +7,15 @@ import guru.springframework.spring5recipeapp.converters.RecipeToRecipeCommand;
 import guru.springframework.spring5recipeapp.model.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class RecipeServiceIT {
 
